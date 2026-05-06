1075806f402097e637ea6510c68bd5b9c055b836
Maxim Pukh <mapooff@gmail.com>
2026-05-06 22:00:06 +0200

    add get one pook endpoint

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Book struct {
@@ -41,34 +42,45 @@ func (app *App) handleBooks(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		app.getBooks(w)
 	case http.MethodPost:
-		app.addBook(w, r)
+		app.createBook(w, r)
 	default:
 		message := fmt.Sprintf("The method %s is not implemented yet", r.Method)
 		http.Error(w, message, http.StatusInternalServerError)
 	}
 }
 
-func (app *App) getBooks(w http.ResponseWriter) {
+func (app *App) handleBook(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		app.getBook(w, r)
+	default:
+		m := fmt.Sprintf("The method %s is not implemented yet", r.Method)
+		http.Error(w, m, http.StatusInternalServerError)
+	}
+}
+
+func writeJSON(w http.ResponseWriter, data any) {
 	w.Header().Set("Content-Type", "application/json")
 
-	err := json.NewEncoder(w).Encode(app.Books)
+	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		fmt.Fprintf(w, "Something went wrong: %s", err.Error())
 		http.Error(w, "my custom error", http.StatusInternalServerError)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusOK)
-
 }
 
-func (app *App) addBook(w http.ResponseWriter, r *http.Request) {
+func (app *App) getBooks(w http.ResponseWriter) {
+	writeJSON(w, app.Books)
+}
+
+func (app *App) createBook(w http.ResponseWriter, r *http.Request) {
 	var input Book
 	err := json.NewDecoder(r.Body).Decode(&input)
 
 	if err != nil {
-		fmt.Fprintf(w, "Something went wrong: %s", err.Error())
+		// fmt.Fprintf(w, "Something went wrong: %s", err.Error())
 		http.Error(w, "my custom error", http.StatusInternalServerError)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -81,9 +93,27 @@ func (app *App) addBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *App) getBook(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Path[len("/book/"):])
+	if err != nil {
+		m := fmt.Sprintf("Can't get book id: %s", err.Error())
+		http.Error(w, m, http.StatusInternalServerError)
+		return
+	}
+
+	_, ok := app.Books[id]
+
+	if ok != true {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	writeJSON(w, app.Books[id])
+}
+
 func main() {
 	app := NewApp()
 
 	http.HandleFunc("/books", app.handleBooks)
+	http.HandleFunc("/book/", app.handleBook)
 	log.Fatal(http.ListenAndServe(":1234", nil))
 }
